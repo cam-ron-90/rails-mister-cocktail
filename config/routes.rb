33276a0bd9c237ed52dcec662bbd2da4d278d79c
Cameron Melville <cam.t.melville@gmail.com>
2020-02-20 21:13:42 +1100

    adding style

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+
+  resources :cocktails do
+    resources :doses, only: [ :new, :create ]
+  end
+
+  resources :doses, only: [:destroy]
+
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
+
+  # get 'doses/new'
+  # get 'doses/create'
+  # delete 'doses/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
